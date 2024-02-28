b48db05fbfe131a69711e6bfebaae17736fc70b7
Scot_Survivor <40865296+Scot-Survivor@users.noreply.github.com>
2024-02-28 12:06:28 +0000

    chore: Add Patient table.

---
+++ src/main/java/com/comp5590/entities/Patient.java
@@ -0,0 +1,36 @@
+package com.comp5590.entities;
+
+import com.comp5590.enums.CommunicationPreference;
+import jakarta.persistence.*;
+
+public class Patient {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    @Column(name = "patientId")
+    private int id;
+
+    // Personal details
+    @Column(name="firstName")
+    private String firstName;
+    @Column(name="surName")
+    private String surName;
+
+    @Column(name="phone")
+    private String phone;
+
+    @Column(name="fax")
+    private String fax;
+
+    @Column(name="additionalNotes")
+    private String additionalNotes;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "communicationPreference")
+    private CommunicationPreference communicationPreference;
+
+    // Authentication Details
+    @Column(name = "email")
+    private String email;
+    @Column(name = "password")
+    private String password;  // This will be base64 encoded hash
+}
---
+++ src/main/java/com/comp5590/enums/CommunicationPreference.java
@@ -0,0 +1,5 @@
+package com.comp5590.enums;
+
+public enum CommunicationPreference {
+    NONE, EMAIL, PHONE, FAX
+}
