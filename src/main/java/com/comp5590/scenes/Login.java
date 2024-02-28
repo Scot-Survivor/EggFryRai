c9436922ce69789632edc01d1a2ad816d59b4ca1
Rhys Walker <rw531@kent.ac.uk>
2024-02-28 14:02:55 +0000

    feat: Added login screen text fields and buttons

---
+++ src/main/java/com/comp5590/scenes/Login.java
@@ -0,0 +1,50 @@
+package com.comp5590.scenes;
+
+import com.comp5590.HelloFX;
+import javafx.scene.control.Button;
+import javafx.scene.layout.*;
+import javafx.scene.text.Text;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+
+public class Login {
+
+    private HelloFX main;
+    private BorderPane mainPane;
+
+    public Login(HelloFX main){ // exception needed for hashing
+        this.main = main;
+
+        // create the border pane object and add our items to it
+        mainPane = new BorderPane();
+        mainPane.setCenter(createLogin());
+    }
+
+    /**
+     * A function that creates a button to send us to the POS screen
+     * @return The button
+     */
+    private VBox createLogin(){
+        Label emailText = new Label("Email:");
+        TextField email = new TextField();
+        Label passwordText = new Label("Password:");
+        PasswordField password = new PasswordField();
+
+        Button loginButton = new Button("Login");
+        loginButton.setOnAction(e -> {
+            System.out.println("Login Clicked!");
+        });
+
+        VBox vbox = new VBox(emailText, email, passwordText, password, loginButton);
+        return vbox;
+    }
+
+    /**
+     * Gets the root node of the given interface
+     * @return BorderPane which is the root node
+     */
+    public BorderPane getRoot(){
+        return mainPane;
+    }
+}
