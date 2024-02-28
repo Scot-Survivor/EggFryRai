36491b96552e4774e839dbc58af5bc2fd53e2607
Rhys Walker <rw531@kent.ac.uk>
2024-02-28 14:59:08 +0000

    feat: Added doctor class to entities. Included the overview.pdf

---
+++ src/main/java/com/comp5590/entities/Doctor.java
@@ -0,0 +1,30 @@
+package com.comp5590.entities;
+
+import com.comp5590.enums.CommunicationPreference;
+import jakarta.persistence.*;
+
+public class Doctor {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    @Column(name = "doctorId")
+    private int doctorId;
+
+    @Column(name = "firstName")
+    private String firstName;
+
+    @Column(name = "surname")
+    private String surname;
+
+    @Column(name="phone")
+    private String phone;
+
+    @Column(name="fax")
+    private String fax;
+
+    @Column(name = "email")
+    private String email;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "communicationPreference")
+    private CommunicationPreference communicationPreference;
+}
