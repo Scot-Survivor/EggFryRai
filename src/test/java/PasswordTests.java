38dd86dec889d3f600f73621d9588cee04d3561e
Scot_Survivor <40865296+Scot-Survivor@users.noreply.github.com>
2024-02-29 14:26:48 +0000

    ci: Introduce invalid password test, fix naming

--- src/test/java/PasswordTests.java
+++ src/test/java/PasswordTests.java
@@ -2,17 +2,27 @@ import com.comp5590.managers.secuirty.ArgonPasswordManager;
 import com.comp5590.managers.secuirty.PasswordManager;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class PasswordTests extends SetupTests {
-    PasswordManager argonPasswordManager = new ArgonPasswordManager();
 
     @Test
-    public void testPasswordHashing() {
+    public void testArgonPasswordValid() {
+        PasswordManager argonPasswordManager = new ArgonPasswordManager();
         argonPasswordManager.initialise();
         String password = "password";
         String hashedPassword = argonPasswordManager.hashPassword(password);
         assertTrue(argonPasswordManager.passwordMatches(hashedPassword, password));
+    }
+
+    @Test
+    public void testArgonPasswordInvalid() {
+        PasswordManager argonPasswordManager = new ArgonPasswordManager();
+        argonPasswordManager.initialise();
+        String password = "password";
+        String hashedPassword = argonPasswordManager.hashPassword(password);
+        assertFalse(argonPasswordManager.passwordMatches(hashedPassword, "password1"));
     }
 
 }
