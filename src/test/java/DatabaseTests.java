de7f8a092dfe67a0e6228da8e6161e88cb4db022
Scot_Survivor <40865296+Scot-Survivor@users.noreply.github.com>
2024-02-29 01:17:33 +0000

    chore: Add some basic tests

--- src/test/java/DatabaseTests.java
+++ src/test/java/DatabaseTests.java
@@ -1,10 +1,13 @@
 import com.comp5590.configuration.AppConfig;
+import com.comp5590.entities.Address;
 import com.comp5590.entities.Doctor;
+import com.comp5590.entities.Patient;
 import com.comp5590.enums.CommunicationPreference;
 import com.comp5590.managers.DatabaseManager;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
@@ -22,10 +25,36 @@ public class DatabaseTests {
     }
 
     @Test
-    public void testDoctorEntity() {
+    public void testDoctorEntityCreation() {
         Doctor doctor = new Doctor("Test", "Test", "", "", "example@example.org",
                 CommunicationPreference.EMAIL);
         DatabaseManager db = DatabaseManager.getInstance();
         assertTrue(db.save(doctor));
+    }
+
+    @Test
+    public void testPatientEntityCreation() {
+        Patient patient = new Patient();
+        DatabaseManager db = DatabaseManager.getInstance();
+        assertFalse(db.save(patient)); // This should fail as there is no address
+    }
+
+    @Test
+    public void testAddressEntityCreation() {
+        Address address = new Address("1234 Example St", "Test", "AB",
+                "12345", "");
+        DatabaseManager db = DatabaseManager.getInstance();
+        assertTrue(db.save(address));
+    }
+
+    @Test
+    public void testPatientWithAddress() {
+        Address address = new Address("1234 Example St", "Test", "AB",
+                "12345", "");
+        Patient patient = new Patient();
+        patient.setAddress(address);
+        DatabaseManager db = DatabaseManager.getInstance();
+        assertTrue(db.save(address));
+        assertTrue(db.save(patient));
     }
 }
