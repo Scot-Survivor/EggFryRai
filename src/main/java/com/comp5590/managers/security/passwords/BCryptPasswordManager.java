e0d5c8d88d9cbb1fc5ab45fb65cea11f6c25657d
Scot_Survivor <40865296+Scot-Survivor@users.noreply.github.com>
2024-03-01 13:53:19 +0000

    feat(passwords): Add bcrypt password manager.

---
+++ src/main/java/com/comp5590/managers/security/passwords/BCryptPasswordManager.java
@@ -0,0 +1,24 @@
+package com.comp5590.managers.security.passwords;
+
+import com.comp5590.configuration.AppConfig;
+import com.comp5590.managers.security.passwords.PasswordManager;
+import org.mindrot.jbcrypt.BCrypt;
+
+public class BCryptPasswordManager extends PasswordManager {
+    private final int rounds = AppConfig.BCRYPT_ROUNDS;
+    @Override
+    public boolean passwordMatches(String storedPassword, String userPassword) {
+        String hashed = decodeBase64(storedPassword);
+        return BCrypt.checkpw(userPassword, hashed);
+    }
+
+    @Override
+    public String hashPassword(String userPassword) {
+        return encodeBase64(BCrypt.hashpw(userPassword, BCrypt.gensalt(rounds)));
+    }
+
+    @Override
+    public void initialise() {
+        available = true;
+    }
+}
