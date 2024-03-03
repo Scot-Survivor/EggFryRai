fb55fe912f1a08f18de3b3e032f4b0026e74e3c6
Scot_Survivor <40865296+Scot-Survivor@users.noreply.github.com>
2024-03-03 22:46:48 +0000

    test(TestFX): Adjust properties inline with the link below
    https://gitlab.stud.idi.ntnu.no/tdt4140-staff/examples/-/issues/7

--- src/test/java/SetupTests.java
+++ src/test/java/SetupTests.java
@@ -6,10 +6,15 @@ public class SetupTests {
     public static void setup() {
         // Set up the logger
         AppConfig.ConfigFile = "src/test/resources/test.properties";
+        System.setProperty("prism.verbose", "true"); // optional
+        System.setProperty("java.awt.headless", "true");
         System.setProperty("testfx.robot", "glass");
         System.setProperty("testfx.headless", "true");
+        System.setProperty("glass.platform", "Monocle");
+        System.setProperty("monocle.platform", "Headless");
         System.setProperty("prism.order", "sw");
         System.setProperty("prism.text", "t2k");
-        System.setProperty("java.awt.headless", "true");
+        System.setProperty("testfx.setup.timeout", "2500");
+
     }
 }
