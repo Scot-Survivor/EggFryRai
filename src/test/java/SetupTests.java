23b2909df5d0fa90565c9f0d281eaa2c5a278350
Scot_Survivor <40865296+Scot-Survivor@users.noreply.github.com>
2024-03-03 22:35:47 +0000

    test(TestFX): Enable Headless testing
    The fix for headless testing was to change the openjfx monocle library to version 17.0.10

--- src/test/java/SetupTests.java
+++ src/test/java/SetupTests.java
@@ -6,5 +6,10 @@ public class SetupTests {
     public static void setup() {
         // Set up the logger
         AppConfig.ConfigFile = "src/test/resources/test.properties";
+        System.setProperty("testfx.robot", "glass");
+        System.setProperty("testfx.headless", "true");
+        System.setProperty("prism.order", "sw");
+        System.setProperty("prism.text", "t2k");
+        System.setProperty("java.awt.headless", "true");
     }
 }
