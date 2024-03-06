ef557d49db850ecaf09e892cd1bdcb5a3d1350e9
Scot_Survivor <40865296+Scot-Survivor@users.noreply.github.com>
2024-03-06 10:37:44 +0000

    test(Doctor): Move Doctor test to its own class

--- src/test/java/com/comp5590/tests/basic/DatabaseTests.java
+++ src/test/java/com/comp5590/tests/basic/DatabaseTests.java
@@ -1,9 +1,7 @@
 package com.comp5590.tests.basic;
 
 import com.comp5590.entities.Address;
-import com.comp5590.entities.Doctor;
 import com.comp5590.entities.Patient;
-import com.comp5590.enums.CommunicationPreference;
 import com.comp5590.managers.DatabaseManager;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
@@ -22,13 +20,6 @@ public class DatabaseTests extends SetupTests {
         assertTrue(db.testConnection());
     }
 
-    @Test
-    public void testDoctorEntityCreation() {
-        Doctor doctor = new Doctor("Test", "Test", "", "", "example@example.org",
-                CommunicationPreference.EMAIL);
-        DatabaseManager db = DatabaseManager.getInstance();
-        assertTrue(db.save(doctor));
-    }
 
     @Test
     public void testPatientEntityCreation() {
