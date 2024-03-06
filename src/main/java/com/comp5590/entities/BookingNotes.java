3ae46c9d5237c40ea28d7bf4cbf99c57c6d612b8
Eclar <ec634@kent.ac.uk>
2024-03-06 13:10:03 +0000

    feat: bookingNotes entity

---
+++ src/main/java/com/comp5590/entities/BookingNotes.java
@@ -0,0 +1,27 @@
+package com.comp5590.entities;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "bookingnotes")
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class BookingNotes {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    @Column(name = "bookingId")
+    private int bookingnotesId;
+
+    @OneToOne
+    @JoinColumn(name="bookingId")
+    private Booking bookingId;
+
+    @Column(name="notes")
+    private String notes;
+
+    @Column(name="prescription")
+    private String prescription;
+}
