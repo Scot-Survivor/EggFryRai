7cb5464a569201fb8c2c9c588d53674cdc7ac97e
Scott Richards <sr845@kent.ac.uk>
2024-03-06 12:06:12 +0000

    feat: added insurance fields.

--- src/main/java/com/comp5590/entities/Insurance.java
+++ src/main/java/com/comp5590/entities/Insurance.java
@@ -3,5 +3,29 @@ package com.comp5590.entities;
 import jakarta.persistence.*;
 import lombok.*;
 
+
+@Entity
+@Table(name = "insurance")
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
 public class Insurance {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    @Column(name = "patientId")
+    private int insuranceId;
+
+    @Column(name="patientIdd")
+    private String patientIdd;
+
+    @Column(name="insuranceProvider")
+    private String insuranceProvider;
+
+    @Column(name="startDate")
+    private String startDate;
+
+    @Column(name="endDate")
+    private String endDate;
 }
