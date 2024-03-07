2ff8f846620de2973fc82b11032389a8d3f9a9fc
Aragorn <80429382+AragornElessar1973@users.noreply.github.com>
2024-03-07 10:09:29 +0000

    fix(LoginScreen): split components into files, add some styling (unfinished)

--- src/main/java/com/comp5590/screens/LoginScreen.java
+++ src/main/java/com/comp5590/screens/LoginScreen.java
@@ -4,9 +4,10 @@ import com.comp5590.entities.Patient;
 import com.comp5590.managers.LoggerManager;
 import com.comp5590.managers.ScreenManager;
 import javafx.event.ActionEvent;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.layout.*;
-import javafx.scene.text.Text;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
@@ -14,6 +15,11 @@ import org.apache.logging.log4j.core.Logger;
 import org.hibernate.Session;
 
 import java.util.List;
+
+import com.comp5590.components.LoginScreen.BigIcon;
+import com.comp5590.components.LoginScreen.Paragraph;
+// import title component
+import com.comp5590.components.LoginScreen.Title;
 
 /**
  * @author Rhys Walker
@@ -24,25 +30,58 @@ public class LoginScreen extends AbstractScreen {
     private Label error;
     private final Logger logger = LoggerManager.getInstance().getLogger(LoginScreen.class);
 
-    public LoginScreen(ScreenManager screenManager){ // exception needed for hashing
+    public LoginScreen(ScreenManager screenManager) { // exception needed for hashing
         super(screenManager);
     }
 
     @Override
     public void setup() {
-        // create the border pane object and add the login to the center
-        BorderPane pane = new BorderPane();
-        pane.setCenter(createLogin());
-        pane.setTop(createTitle());
-        setRootPane(pane);
+        // load custom CSS
         this.cssPath = "/login.css";
+
+        // create the grid pane object, apply styling & properties to it
+        GridPane pane = new GridPane();
+        pane.getStyleClass().add("custom-pane");
+        pane.setPrefSize(250, 50);
+        pane.setMaxSize(300, 400);
+        pane.setAlignment(Pos.CENTER);
+        pane.setPadding(new Insets(20, 20, 20, 20)); // set the padding around the grid pane
+        pane.setHgap(10); // set the horizontal gap between columns
+        pane.setVgap(10); // set the vertical gap between rows
+
+        // create child components, imported from the components folder
+        HBox titleBox = new Title("Login");
+        HBox paragraph = new Paragraph("Please login to track your appointments, prescriptions, and more.");
+        VBox icon = new BigIcon("src/main/public/healthcare.png"); // goddamn disgusting image path took ages to fix
+        VBox loginBox = createLogin();
+
+        // add child components to our grid pane
+        pane.add(titleBox, 0, 0);
+        pane.add(paragraph, 0, 1);
+        pane.add(icon, 1, 0);
+        GridPane.setRowSpan(icon, 2);
+        pane.add(loginBox, 0, 2);
+        GridPane.setColumnSpan(loginBox, 2);
+
+        // align the grid pane's contents to the center of the screen
+        GridPane.setHalignment(titleBox, javafx.geometry.HPos.LEFT);
+        GridPane.setHalignment(loginBox, javafx.geometry.HPos.CENTER);
+        GridPane.setValignment(loginBox, javafx.geometry.VPos.CENTER);
+
+        // create the border pane (which will serve as root pane)
+        // set grid pane as child of border pane
+        BorderPane rootPane = new BorderPane();
+        rootPane.setCenter(pane);
+
+        setRootPane(rootPane); // set root pane
     }
 
     /**
      * A function that creates a button to send us to the POS screen
+     *
      * @return The button
      */
-    private VBox createLogin(){
+    private VBox createLogin() {
         Label emailText = new Label("Email:");
         this.email = new TextField();
         this.email.setId("email");
@@ -56,10 +95,13 @@ public class LoginScreen extends AbstractScreen {
         loginButton.setId("login");
         loginButton.setOnAction(this::login);
 
-        return new VBox(emailText, email, passwordText, password, loginButton, this.error);
+        VBox box = new VBox(emailText, email, passwordText, password, loginButton, this.error);
+        box.setSpacing(10);
+
+        return box;
     }
 
-    private void login(ActionEvent event){
+    private void login(ActionEvent event) {
         String email = this.email.getText();
         String password = this.password.getText();
         Session session = getSessionFactory().openSession();
@@ -67,14 +109,14 @@ public class LoginScreen extends AbstractScreen {
                 .setParameter("email", email)
                 .list();
         session.close();
-        if (patients.isEmpty()){
+        if (patients.isEmpty()) {
             logger.error("Invalid Username({})", email);
             this.error.setText("Invalid Username or Password");
             return;
         }
         Patient patient = patients.get(0);
-        boolean passwordValid  = getApp().getPasswordManager().passwordMatches(patient.getPassword(), password);
-        if (passwordValid){
+        boolean passwordValid = getApp().getPasswordManager().passwordMatches(patient.getPassword(), password);
+        if (passwordValid) {
             if (!patient.isTwoFactorEnabled()) {
                 getApp().getScreenManager().showScene(HomeScreen.class);
             } else {
@@ -86,15 +128,6 @@ public class LoginScreen extends AbstractScreen {
             logger.error("Invalid Password(*)");
             this.error.setText("Invalid Username or Password");
         }
-    }
-
-    // Create title
-    private HBox createTitle(){
-        Text text = new Text("Please Login");
-        text.getStyleClass().add("title-text");
-        HBox titleBox = new HBox(text);
-        titleBox.getStyleClass().add("title");
-        return titleBox;
     }
 
     public void setErrorText(String txt) {
