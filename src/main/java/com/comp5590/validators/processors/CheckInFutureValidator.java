041574b7a94bc5dd2cb006032d964a593f681ee2
Aragorn <80429382+AragornElessar1973@users.noreply.github.com>
2024-03-07 18:24:24 +0000

    feat(InFuture validator): Add InFuture validation annotation to apptTime field for Booking entity

---
+++ src/main/java/com/comp5590/validators/processors/CheckInFutureValidator.java
@@ -0,0 +1,16 @@
+package com.comp5590.validators.processors;
+
+import com.comp5590.validators.annontations.InFuture;
+import jakarta.validation.ConstraintValidator;
+import java.util.Date;
+
+public class CheckInFutureValidator implements ConstraintValidator<InFuture, Date> {
+  @Override
+  public boolean isValid(Date date, jakarta.validation.ConstraintValidatorContext constraintValidatorContext) {
+    // grab current time
+    long currentTime = System.currentTimeMillis();
+
+    // check if the appointment time is in the future
+    return currentTime < date.getTime();
+  }
+}
