eb02c80dc58b18c60a16907c9a62a0423ee3cf65
Aragorn <80429382+AragornElessar1973@users.noreply.github.com>
2024-03-07 19:44:51 +0000

    test(ChronologicalDates validator): Ensure that the Insurance entity date properties annotations work

---
+++ src/test/java/com/comp5590/tests/validators/ChronologicalDatesValidatorTests.java
@@ -0,0 +1,79 @@
+package com.comp5590.tests.validators;
+
+import com.comp5590.entities.Insurance;
+import com.comp5590.tests.basic.SetupTests;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import org.hibernate.validator.HibernateValidator;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Date;
+
+public class ChronologicalDatesValidatorTests extends SetupTests {
+
+  @Test
+  public void testStartDateBeforeEndDate() {
+    Validator validator = Validation.byProvider(HibernateValidator.class)
+        .configure()
+        .buildValidatorFactory()
+        .getValidator();
+
+    Insurance insurance = new Insurance();
+
+    // make 2 dates, one for the start date and one for the end date
+    Date dateStart = Date.from(LocalDate.of(2022, 12, 25).atStartOfDay(ZoneId.systemDefault()).toInstant());
+    Date dateEnd = Date.from(LocalDate.of(2023, 12, 25).atStartOfDay(ZoneId.systemDefault()).toInstant());
+
+    insurance.setStartDate(dateStart);
+    insurance.setEndDate(dateEnd);
+
+    // check if the start date is before the end date
+    assertTrue(validator.validate(insurance).isEmpty());
+  }
+
+  @Test
+  public void testStartDateAfterEndDate() {
+    Validator validator = Validation.byProvider(HibernateValidator.class)
+        .configure()
+        .buildValidatorFactory()
+        .getValidator();
+
+    Insurance insurance = new Insurance();
+
+    // make 2 dates, one for the start date and one for the end date
+    Date dateStart = Date.from(LocalDate.of(2023, 12, 25).atStartOfDay(ZoneId.systemDefault()).toInstant());
+    Date dateEnd = Date.from(LocalDate.of(2022, 12, 25).atStartOfDay(ZoneId.systemDefault()).toInstant());
+
+    insurance.setStartDate(dateStart);
+    insurance.setEndDate(dateEnd);
+
+    // check if the start date is before the end date
+    assertFalse(validator.validate(insurance).isEmpty());
+  }
+
+  @Test
+  public void testStartDateEqualsEndDate() {
+    Validator validator = Validation.byProvider(HibernateValidator.class)
+        .configure()
+        .buildValidatorFactory()
+        .getValidator();
+
+    Insurance insurance = new Insurance();
+
+    // make 2 dates, one for the start date and one for the end date
+    Date dateStart = Date.from(LocalDate.of(2022, 12, 25).atStartOfDay(ZoneId.systemDefault()).toInstant());
+    Date dateEnd = Date.from(LocalDate.of(2022, 12, 25).atStartOfDay(ZoneId.systemDefault()).toInstant());
+
+    insurance.setStartDate(dateStart);
+    insurance.setEndDate(dateEnd);
+
+    // check if the start date is before the end date (should return false cos they
+    // are equal)
+    assertFalse(validator.validate(insurance).isEmpty());
+  }
+}
