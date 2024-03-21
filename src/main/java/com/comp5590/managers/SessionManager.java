5f1579a567f9abc351aab8522de37904ff3f0a89
Eclar <ec634@kent.ac.uk>
2024-03-21 18:20:05 +0000

    feat(session manager): added unauthentificate after amount of time

--- src/main/java/com/comp5590/managers/SessionManager.java
+++ src/main/java/com/comp5590/managers/SessionManager.java
@@ -45,5 +45,19 @@ public class SessionManager {
         // } catch (InterruptedException e) {
         // e.printStackTrace();
         // }
+
+        // Create thread to handle timeout
+        new Thread(() -> {
+            try {
+                // sleep for this amount of hours
+                Thread.sleep(hours * 60 * 60 * 1000);
+                // after sleep set authenticated to false
+                    this.authenticated = false;
+                }
+                catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }).start();
+        }
     }
-}
+
