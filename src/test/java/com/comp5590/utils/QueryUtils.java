f3fb8da20d78b98854091724f089b5eca42841f3
Aragorn <80429382+AragornElessar1973@users.noreply.github.com>
2024-03-25 01:51:44 +0000

    chore(QueryUtils): implement greater range of utils for ease of querying using robot (for tests)

--- src/test/java/com/comp5590/utils/QueryUtils.java
+++ src/test/java/com/comp5590/utils/QueryUtils.java
@@ -8,4 +8,24 @@ public class QueryUtils {
     public static Class<HBox> getHBoxClass() {
         return javafx.scene.layout.HBox.class;
     }
+
+    // for Button
+    public static Class<javafx.scene.control.Button> getButtonClass() {
+        return javafx.scene.control.Button.class;
+    }
+
+    // for textFields
+    public static Class<javafx.scene.control.TextField> getTextFieldClass() {
+        return javafx.scene.control.TextField.class;
+    }
+
+    // for passwordFields
+    public static Class<javafx.scene.control.PasswordField> getPasswordFieldClass() {
+        return javafx.scene.control.PasswordField.class;
+    }
+
+    // for labels
+    public static Class<javafx.scene.control.Label> getLabelClass() {
+        return javafx.scene.control.Label.class;
+    }
 }
