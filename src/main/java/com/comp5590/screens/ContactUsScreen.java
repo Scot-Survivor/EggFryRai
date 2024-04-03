47702a29d1ba01e7ba7fb8a2f3ce2ce36434dc15
Scott Richards <sr845@kent.ac.uk>
2024-04-03 20:10:18 +0100

    feat(contactUs): Added form functionality, with result messages

--- src/main/java/com/comp5590/screens/ContactUsScreen.java
+++ src/main/java/com/comp5590/screens/ContactUsScreen.java
@@ -2,6 +2,7 @@ package com.comp5590.screens;
 
 import com.comp5590.components.LoginScreen.Title;
 import com.comp5590.managers.ScreenManager;
+import javafx.event.ActionEvent;
 import javafx.geometry.Pos;
 import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
@@ -11,6 +12,13 @@ import lombok.Getter;
 
 @Getter
 public class ContactUsScreen extends AbstractScreen {
+
+    // Define fields in class so submission can access them
+    private TextField firstName;
+    private TextField lastName;
+    private TextField email;
+    private TextArea message;
+    private VBox formBox;
 
     public ContactUsScreen(ScreenManager screenManager) {
         super(screenManager);
@@ -38,16 +46,17 @@ public class ContactUsScreen extends AbstractScreen {
         // Create button
         Button submitButton = new Button("Submit");
         submitButton.setId("submitButton");
+        submitButton.setOnAction(this::submission);
 
         // Create text fields and labels
         Label firstNameLabel = new Label("First name");
-        TextField firstName = new TextField();
+        firstName = new TextField();
         Label lastNameLabel = new Label("Last name");
-        TextField lastName = new TextField();
+        lastName = new TextField();
         Label emailLabel = new Label("Email");
-        TextField email = new TextField();
+        email = new TextField();
         Label messageLabel = new Label("What is it you need assistance with?");
-        TextArea message = new TextArea(); // Text area so input wraps
+        message = new TextArea(); // Text area so input wraps
 
         Label bottomNote = new Label("Please note a response may take up to 5 working days.");
 
@@ -56,7 +65,9 @@ public class ContactUsScreen extends AbstractScreen {
         info.setId("infoLabel");
         bottomNote.setId("noteLabel");
 
-        VBox formBox = new VBox(
+        // Create form
+        formBox =
+        new VBox(
             firstNameLabel,
             firstName,
             lastNameLabel,
@@ -79,6 +90,51 @@ public class ContactUsScreen extends AbstractScreen {
         center.setMaxSize(600, 600);
         center.setAlignment(Pos.CENTER);
         return center;
+    }
+
+    private void submission(ActionEvent event) {
+        // Boolean used to check if any values empty
+        boolean filled = true;
+        // Check each field, if empty highlight red, else remove highlighting
+        if (firstName.getText().isEmpty()) {
+            firstName.setStyle("-fx-effect: dropshadow(gaussian, rgba(255,0,0,0.5), 10, 0, 0, 0);");
+            filled = false;
+        } else {
+            firstName.setStyle("-fx-effect: '';");
+        }
+        if (lastName.getText().isEmpty()) {
+            lastName.setStyle("-fx-effect: dropshadow(gaussian, rgba(255,0,0,0.5), 10, 0, 0, 0);");
+            filled = false;
+        } else {
+            lastName.setStyle("-fx-effect: '';");
+        }
+        if (email.getText().isEmpty()) {
+            email.setStyle("-fx-effect: dropshadow(gaussian, rgba(255,0,0,0.5), 10, 0, 0, 0);");
+            filled = false;
+        } else {
+            email.setStyle("-fx-effect: '';");
+        }
+        if (message.getText().isEmpty()) {
+            message.setStyle("-fx-effect: dropshadow(gaussian, rgba(255,0,0,0.5), 10, 0, 0, 0);");
+            filled = false;
+        } else {
+            message.setStyle("-fx-effect: '';");
+        }
+
+        // Check if filled or not, print result message
+        if (!filled) {
+            Label failed = new Label("You are missing details.");
+            failed.setStyle("-fx-text-fill: red");
+            formBox.getChildren().add(failed);
+        } else {
+            Label success = new Label("Your details have been sent.");
+            success.setStyle("-fx-text-fill: green");
+            formBox.getChildren().add(success);
+            firstName.clear();
+            lastName.clear();
+            email.clear();
+            message.clear();
+        }
     }
 
     @Override
