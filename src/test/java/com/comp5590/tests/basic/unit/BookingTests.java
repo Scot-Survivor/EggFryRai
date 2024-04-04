f1193996f962a7fbe1c5c06eda82d4d85d82822a
Rhysw <rw531@kent.ac.uk>
2024-04-04 14:26:02 +0100

    chore(Test): Re wrote the test to allow for other tests to be added

--- src/test/java/com/comp5590/tests/basic/unit/BookingTests.java
+++ src/test/java/com/comp5590/tests/basic/unit/BookingTests.java
@@ -7,15 +7,19 @@ import com.comp5590.database.entities.*;
 import com.comp5590.database.managers.DatabaseManager;
 import com.comp5590.enums.UserRole;
 import com.comp5590.tests.basic.SetupTests;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.Date;
 import org.junit.jupiter.api.Test;
 
 public class BookingTests extends SetupTests {
 
-    @Test
-    public void testBookingEntityCreation() throws ParseException {
+    /**
+     * Create a booking entity given a roomNum
+     * @param roomNum The room number to use
+     * @return A Booking entity
+     */
+    private Booking makeBooking(String roomNum) {
         // create a db manager
         DatabaseManager db = DatabaseManager.getInstance();
 
@@ -26,24 +30,33 @@ public class BookingTests extends SetupTests {
         User doctor = createPatient("example1@example.com", "password");
         doctor.setRole(UserRole.DOCTOR);
 
-        // create a date and time for the appt
-        SimpleDateFormat formatDate = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        Date apptTime = formatDate.parse("2025-01-05 00:00:00");
+        // Use current time plus two weeks to make test more valid for future
+        LocalDateTime currentTime = LocalDateTime.now();
+        LocalDateTime futureDateTime = currentTime.plus(2, ChronoUnit.WEEKS);
+        Date twoWeeks = java.sql.Timestamp.valueOf(futureDateTime);
 
         // make and save room
-        Room room = makeRoom("1", createAddress());
+        Room room = makeRoom(roomNum, createAddress());
         assertNotNull(room);
 
         // create booking object
         Booking booking = new Booking();
         booking.setDoctor(doctor);
         booking.setPatient(patient);
-        booking.setApptTime(apptTime);
+        booking.setApptTime(twoWeeks);
         booking.setRoom(room);
 
+        return booking;
+    }
+
+    /**
+     * Test the creation of a booking entity on its own
+     */
+    @Test
+    public void testBookingEntityCreation() {
+        DatabaseManager db = DatabaseManager.getInstance();
+
         // check saving of booking was successfully
-        assertTrue(db.save(booking));
-        SetupTests.remove(patient);
-        SetupTests.remove(doctor);
+        assertTrue(db.save(makeBooking("1")));
     }
 }
