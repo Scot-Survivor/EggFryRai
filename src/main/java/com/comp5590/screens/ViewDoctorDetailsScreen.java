03e4ca408a965d1685b1ad7fd6f82b4df589ec75
Rhysw <rw531@kent.ac.uk>
2024-04-04 13:30:41 +0100

    feat(Error): Error logging and message displayed to user when no doctor is selected

--- src/main/java/com/comp5590/screens/ViewDoctorDetailsScreen.java
+++ src/main/java/com/comp5590/screens/ViewDoctorDetailsScreen.java
@@ -5,6 +5,7 @@ import com.comp5590.components.DoctorDetailsScreen.DoctorDisplayBox;
 import com.comp5590.components.DoctorDetailsScreen.Title;
 import com.comp5590.database.entities.User;
 import com.comp5590.database.managers.DatabaseManager;
+import com.comp5590.managers.LoggerManager;
 import com.comp5590.managers.ScreenManager;
 import com.comp5590.security.managers.authentication.annotations.AuthRequired;
 import java.util.HashMap;
@@ -16,13 +17,13 @@ import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
-
-// TODO: Make the items look pretty
+import org.apache.logging.log4j.core.Logger;
 
 @AuthRequired
 public class ViewDoctorDetailsScreen extends AbstractScreen {
 
     private HashMap<String, User> doctorMap; // Will contain a mapping of doctors names to their object
+    private final Logger logger = LoggerManager.getInstance().getLogger(ViewDoctorDetailsScreen.class);
 
     public ViewDoctorDetailsScreen(ScreenManager screenManager) {
         super(screenManager);
@@ -56,38 +57,39 @@ public class ViewDoctorDetailsScreen extends AbstractScreen {
         showDoctor.setId("showButton");
         VBox.setMargin(showDoctor, new Insets(10, 10, 10, 10));
         showDoctor.setOnAction(e -> {
-            Pane rootPane = getRootPane();
-
-            // get the choice of doctor to look at
-            ChoiceBox<String> doc = (ChoiceBox<String>) rootPane.lookup("#doctorChoiceBox");
-            User doctor = doctorMap.get(doc.getValue());
-
-            // get all of the details and format
-            String fullName = "Name: " + doctor.getFirstName() + " " + doctor.getSurName() + "\n";
-            String number = "Phone no: " + doctor.getPhone() + "\n";
-            String email = "Email Address: " + doctor.getAuthenticationDetails().getEmail() + "\n";
-            String fax = "Fax no: " + doctor.getFax() + "\n";
-            String contactPreference = "Contact Preference: " + doctor.getCommunicationPreference() + "\n";
-            String addNotes = "Additional Notes: " + doctor.getAdditionalNotes() + "\n";
-
-            // find the box to display
-            Pane root = getRootPane();
-            VBox doctorInfo = (VBox) root.lookup("#docDisplay");
-
+            Pane rootPane = getRootPane(); // get root pane for any lookups
+            VBox doctorInfo = (VBox) rootPane.lookup("#docDisplay"); // get now to save multiple calls
             // clear prior contents
             doctorInfo.getChildren().clear();
 
-            // add to the box
-            doctorInfo
-                .getChildren()
-                .addAll(
-                    new Text(fullName),
-                    new Text(number),
-                    new Text(email),
-                    new Text(fax),
-                    new Text(contactPreference),
-                    new Text(addNotes)
-                );
+            try {
+                // get the choice of doctor to look at
+                ChoiceBox<String> doc = (ChoiceBox<String>) rootPane.lookup("#doctorChoiceBox");
+                User doctor = doctorMap.get(doc.getValue());
+
+                // get all of the details and format
+                String fullName = "Name: " + doctor.getFirstName() + " " + doctor.getSurName() + "\n";
+                String number = "Phone no: " + doctor.getPhone() + "\n";
+                String email = "Email Address: " + doctor.getAuthenticationDetails().getEmail() + "\n";
+                String fax = "Fax no: " + doctor.getFax() + "\n";
+                String contactPreference = "Contact Preference: " + doctor.getCommunicationPreference() + "\n";
+                String addNotes = "Additional Notes: " + doctor.getAdditionalNotes() + "\n";
+
+                // add to the box
+                doctorInfo
+                    .getChildren()
+                    .addAll(
+                        new Text(fullName),
+                        new Text(number),
+                        new Text(email),
+                        new Text(fax),
+                        new Text(contactPreference),
+                        new Text(addNotes)
+                    );
+            } catch (Exception error) {
+                logger.error("No doctor selected | " + error);
+                doctorInfo.getChildren().add(new Text("Please select a doctor"));
+            }
         });
 
         // Add all components to the screen
