87bf6b335771f8aa3d172a90ff82f4ef68f67de6
Rhysw <rw531@kent.ac.uk>
2024-04-04 13:49:36 +0100

    feat(Test): Added a new test to check if the error message is displayed correctly

--- src/test/java/com/comp5590/tests/integration/ViewDoctorDetailsTest.java
+++ src/test/java/com/comp5590/tests/integration/ViewDoctorDetailsTest.java
@@ -67,11 +67,39 @@ public class ViewDoctorDetailsTest extends SetupTests {
 
             // Loop over and check that all of the items are text and not null
             DoctorDisplayBox docDisplay = robot.lookup("#docDisplay").queryAs(DoctorDisplayBox.class);
+            assertTrue(!docDisplay.getChildren().isEmpty()); // Make sure not empty
             for (javafx.scene.Node node : docDisplay.getChildren()) {
                 if (node instanceof Text) {
                     Text text = (Text) node;
-                    assertNotNull(text);
+                    assertNotNull(text); // Make sure text is not null
                 }
+            }
+        });
+    }
+
+    /**
+     * Test to check if the error message displays correctly for when no doctor is selected
+     * @param robot
+     */
+    @Test
+    public void testErrorDisplay(FxRobot robot) {
+        // goToScreen(app, robot, CreateBooking.class);
+        // We have to ensure we're a patient logged in
+        User user = createPatient("testPatient1@example.com", "testPassword");
+        this.loginUser(this.app, robot, "testPatient1@example.com", "testPassword");
+        assertEquals(SessionManager.getInstance().getCurrentUser().getId(), user.getId());
+
+        // Select a doctor
+        robot.interact(() -> {
+            app.getScreenManager().showScene(ViewDoctorDetailsScreen.class);
+            assertInstanceOf(ViewDoctorDetailsScreen.class, app.getScreenManager().getCurrentScreen()); // Check we are on the right screen
+
+            robot.lookup("#showButton").queryAs(Button.class).fire();
+            stall(robot);
+
+            DoctorDisplayBox errorText = robot.lookup("#docDisplay").queryAs(DoctorDisplayBox.class);
+            if (errorText.getChildren().size() == 1) {
+                assertEquals("Please select a doctor", ((Text) errorText.getChildren().get(0)).getText());
             }
         });
     }
