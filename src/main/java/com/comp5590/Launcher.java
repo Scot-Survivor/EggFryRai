eaf668cdb6f4237e363e15dca286bdcdbe742fb5
Aragorn <80429382+AragornElessar1973@users.noreply.github.com>
2024-04-05 06:45:56 +0100

    chore(ConfigProperties): create example config properties file for qol dev & fix comment

--- src/main/java/com/comp5590/Launcher.java
+++ src/main/java/com/comp5590/Launcher.java
@@ -1,9 +1,11 @@
 package com.comp5590;
 
 /**
- * This main class is needed to trick Gradle and Java into thinking we're not extending "Application" from JavaFX
- * <a href="https://github.com/javafxports/openjdk-jfx/issues/236#issuecomment-426583174">
- *     This comment outlines the solution
+ * This main class is needed to trick Gradle and Java into thinking we're not
+ * extending "Application" from JavaFX
+ * <a href=
+ * "https://github.com/javafxports/openjdk-jfx/issues/236#issuecomment-426583174">
+ * This comment outlines the solution
  * </a>
  */
 public class Launcher {
