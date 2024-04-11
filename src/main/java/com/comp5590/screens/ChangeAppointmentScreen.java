d6f47c0c35524deb09dccd3a47ed29bbc18da98d
Aragorn <80429382+AragornElessar1973@users.noreply.github.com>
2024-04-11 15:01:03 +0100

    refactor(Screens): refactor every screen to use the new header bar & navbar method. Adjust code accordingly

--- src/main/java/com/comp5590/screens/ChangeAppointmentScreen.java
+++ src/main/java/com/comp5590/screens/ChangeAppointmentScreen.java
@@ -2,30 +2,31 @@ package com.comp5590.screens;
 
 import com.comp5590.managers.LoggerManager;
 import com.comp5590.managers.ScreenManager;
-import com.comp5590.security.managers.authentication.annotations.AuthRequired;
 import javafx.scene.layout.GridPane;
 import org.apache.logging.log4j.core.Logger;
 
-@AuthRequired
-public class ProfileScreen extends AbstractScreen {
+public class ChangeAppointmentScreen extends AbstractScreen {
 
-    private final Logger logger = LoggerManager.getInstance().getLogger(ProfileScreen.class);
+    private final Logger logger = LoggerManager.getInstance().getLogger(ChangeAppointmentScreen.class);
 
-    public ProfileScreen(ScreenManager screenManager) {
+    public ChangeAppointmentScreen(ScreenManager screenManager) {
         super(screenManager);
     }
 
     @Override
     public void setup() {
-        this.addCss("/profile.css");
+        // Load custom css
+        // this.addCss("/newAppointment.css");
 
         // attach default pane, but grab the reference to the gridpane (set as
         // center of borderpane) for further customization
         GridPane pane = this.attachDefaultPane();
         // attach header and nav bar
-        this.attachHeaderAndNavBar("Your Profile");
+        this.attachHeaderAndNavBar("Change Appointment");
     }
 
     @Override
-    public void cleanup() {}
+    public void cleanup() {
+        // nothing to clean up
+    }
 }
