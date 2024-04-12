e66ce12e967d4d78a60a175fc414fef77d7aecf9
Josh Shiells <4584-js2313@users.noreply.git.cs.kent.ac.uk>
2024-04-12 16:47:11 +0000

    Revert "Merge branch 'ProfileScreen' into 'main'"
    This reverts merge request !90

--- src/main/java/com/comp5590/screens/ProfileScreen.java
+++ src/main/java/com/comp5590/screens/ProfileScreen.java
@@ -1,20 +1,15 @@
 package com.comp5590.screens;
 
-import com.comp5590.database.entities.AuthenticationDetails;
-import com.comp5590.database.entities.User;
+import com.comp5590.managers.LoggerManager;
 import com.comp5590.managers.ScreenManager;
-import com.comp5590.managers.SessionManager;
 import com.comp5590.security.managers.authentication.annotations.AuthRequired;
-import javafx.scene.control.Button;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
-import javafx.scene.text.Text;
+import org.apache.logging.log4j.core.Logger;
 
 @AuthRequired
 public class ProfileScreen extends AbstractScreen {
 
-    private final SessionManager sessionManager = SessionManager.getInstance();
+    private final Logger logger = LoggerManager.getInstance().getLogger(ProfileScreen.class);
 
     public ProfileScreen(ScreenManager screenManager) {
         super(screenManager);
@@ -22,72 +17,13 @@ public class ProfileScreen extends AbstractScreen {
 
     @Override
     public void setup() {
-        GridPane pane = attachDefaultPane();
-        attachHeaderAndNavBar("Your Profile");
+        this.addCss("/profile.css");
 
-        // display user information
-        User currentUser = sessionManager.getCurrentUser();
-        AuthenticationDetails authDetails = currentUser.getAuthenticationDetails();
-
-        Text fullNameText = new Text("Full Name: " + sessionManager.getFullName());
-        Text phoneText = new Text("Phone: " + currentUser.getPhone());
-        Text faxText = new Text("Fax: " + currentUser.getFax());
-        Text addressLine1Text = new Text("Address Line 1: " + currentUser.getAddress().getAddressLineOne());
-        Text addressLine2Text = new Text("Town/City: " + currentUser.getAddress().getAddressLineTwo());
-        Text addressLine3Text = new Text("County: " + currentUser.getAddress().getAddressLineThree());
-        Text addressCountryText = new Text("Country: " + currentUser.getAddress().getCountry());
-        Text addressPCText = new Text("Postcode: " + currentUser.getAddress().getPostCode());
-        Text emailText = new Text("Email: " + authDetails.getEmail());
-
-        TextField newEmailField = new TextField();
-        newEmailField.setPromptText("New Email");
-        newEmailField.setId("newEmailField");
-
-        PasswordField newPasswordField = new PasswordField();
-        newPasswordField.setPromptText("New Password");
-        newPasswordField.setId("newPasswordField");
-
-        Button applyEmailButton = new Button("Apply Email");
-        applyEmailButton.setId("applyEmailButton");
-        applyEmailButton.setOnAction(event -> {
-            String newEmail = newEmailField.getText().trim();
-            if (!newEmail.isEmpty()) {
-                authDetails.setEmail(newEmail);
-                // save the updated authentication details
-                getDatabaseManager().update(authDetails);
-                // notify user of successful update and add to logs
-                System.out.println("Email change successful.");
-                getLogger().info("Email change successful: New email is {}", newEmail);
-            }
-        });
-
-        Button applyPasswordButton = new Button("Apply Password");
-        applyPasswordButton.setId("applyPasswordButton");
-        applyPasswordButton.setOnAction(event -> {
-            String newPassword = newPasswordField.getText().trim();
-            if (!newPassword.isEmpty()) {
-                authDetails.setPassword(newPassword);
-                // save the updated authentication details
-                getDatabaseManager().update(authDetails);
-                // notify user of successful update and add to logs
-                System.out.println("Password change successful.");
-                getLogger().info("Password change successful.");
-            }
-        });
-
-        pane.add(fullNameText, 0, 3);
-        pane.add(phoneText, 0, 4);
-        pane.add(faxText, 0, 5);
-        pane.add(addressLine1Text, 0, 6);
-        pane.add(addressLine2Text, 0, 7);
-        pane.add(addressLine3Text, 0, 8);
-        pane.add(addressCountryText, 0, 9);
-        pane.add(addressPCText, 0, 10);
-        pane.add(emailText, 0, 11);
-        pane.add(newEmailField, 0, 12);
-        pane.add(applyEmailButton, 1, 12);
-        pane.add(newPasswordField, 0, 13);
-        pane.add(applyPasswordButton, 1, 13);
+        // attach default pane, but grab the reference to the gridpane (set as
+        // center of borderpane) for further customization
+        GridPane pane = this.attachDefaultPane();
+        // attach header and nav bar
+        this.attachHeaderAndNavBar("Your Profile");
     }
 
     @Override
