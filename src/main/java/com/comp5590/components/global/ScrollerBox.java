fa344bed7392ce8828fb87b90c17757d75c6b419
Aragorn <80429382+AragornElessar1973@users.noreply.github.com>
2024-04-12 07:24:45 +0100

    feat(ScrollerBox): create reusable scroller box component for scrolling infinitely through N nodes

---
+++ src/main/java/com/comp5590/components/global/ScrollerBox.java
@@ -0,0 +1,13 @@
+package com.comp5590.components.global;
+
+import javafx.scene.Node;
+import javafx.scene.layout.VBox;
+
+public class ScrollerBox extends VBox {
+
+    public ScrollerBox(Node... nodes) {
+        this.getStylesheets().add("/global.css");
+
+        this.getChildren().addAll(nodes);
+    }
+}
